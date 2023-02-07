79af52ea4f03fe4baef7692b45a16676e44a6ead
KalebIsaias <kaleb.carvalho@sou.inteli.edu.br>
2023-02-07 09:12:43 -0300

    Jogo limpo

--- Protótipo 01/Scripts/Coin 1.gd
+++ Protótipo 01/Scripts/Coin 1.gd
@@ -1,5 +1,16 @@
 extends Area2D
 
-func _on_Coin_body_entered(body):
-	if body.name == "Player":
-		queue_free()
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
