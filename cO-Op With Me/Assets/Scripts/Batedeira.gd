f1585105b2324d7ee32e51bcd3e45122a7bd2d14
LauroEmm <lauro.rafael@sou.inteli.edu.br>
2023-02-28 18:30:45 -0300

    .

--- cO-Op With Me/Assets/Scripts/Batedeira.gd
+++ cO-Op With Me/Assets/Scripts/Batedeira.gd
@@ -17,6 +17,20 @@ func _on_Batedeira_body_exited(body):
 func _process(_delta):
 	# Botão de interação com o objeto, só funciona quando o jogador aperta o "E", está na area e pegou o ovo.
 	if Input.is_action_just_pressed("action") and in_area == 1 and Global.pegou_ovo == 1:
-		Global.hide_slot1 = 0
+		Global.hide_slot = 0
 		Global.pegou_ovo = 0
 		Global.tem_ovo = 0
+	# Botão de interação com o objeto, só funciona quando o jogador aperta o "E", está na area e pegou o chocolate.
+	if Input.is_action_just_pressed("action") and in_area == 1 and Global.pegou_leite == 1:
+		Global.hide_slot = 0
+		Global.pegou_leite = 0
+		Global.tem_leite = 0
+	if Input.is_action_just_pressed("action") and in_area == 1 and Global.pegou_trigo == 1:
+		Global.hide_slot = 0
+		Global.pegou_trigo = 0
+		Global.tem_trigo = 0
+	if Input.is_action_just_pressed("action") and in_area == 1 and Global.pegou_choco == 1:
+		Global.hide_slot = 0
+		Global.pegou_choco = 0
+		Global.tem_choco = 0
+
