1d611d79567c97a61df180fc6f2a35d887d15216
LauroEmm <lauro.rafael@sou.inteli.edu.br>
2023-03-06 15:29:33 -0300

    Cenas juntos

--- cO-Op With Me/Leite.gd
+++
@@ -1,9 +0,0 @@
-# Script do item leite
-extends TextureRect
-
-func _process(delta):
-# Condional para aparecer ou desaparecer na hotbar do jagador.
-	if Global.hide_slot == 1:
-		$Texture.visible = true
-	else:
-		$Texture.visible = false
