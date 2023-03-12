da03ecdf04e8141b74e1106201bc0b5488610ec9
KalebIsaias <kaleb.carvalho@sou.inteli.edu.br>
2023-03-12 13:48:14 -0300

    Excluindo tudo

--- docs/cO-Op With Me/Assets/Scripts/ControlsScreen.gd
+++
@@ -1,9 +0,0 @@
-#Script da tela de controles
-extends Node2D
-
-func _ready():
-	$"Button(return)".grab_focus()
-
-
-func _on_Buttonreturn_pressed():
-	get_tree().change_scene ("res://Assets/Scenes/StartScreen.tscn")
